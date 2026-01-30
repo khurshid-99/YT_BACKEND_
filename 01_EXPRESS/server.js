787c1c2b55b1dcd4ab2cff5ae7e036914653f504
Khurshid Alam <khurshid1556@gmail.com>
2026-01-30 23:14:26 +0530

    Middlewares basic

--- 01_EXPRESS/server.js
+++ 01_EXPRESS/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("middlewear");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
